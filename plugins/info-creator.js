05087e35eb8ca9ce4626d3e619226f3711474809
Azkaazhrzlva <143519385+Azkaazhrzlva@users.noreply.github.com>
2023-10-20 01:20:26 +0700

    Update info-creator.js

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -4,15 +4,9 @@ let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? c
 let name = await conn.getName(who)
 
   const sentMsg = await conn.sendContactArray(m.chat, [
-    [`${nomorwa}`, `${await conn.getName(nomorwa+'@s.whatsapp.net')}`, `Xyroinee-ID `, `My Skill Just Copas`, `Admin@xyroinee.xyz`, `Indonesia`, `https://api.xyroinee.xyz`, `Developer Clara - MD`],
-    [`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `Whatsapp Bot`, `Jangan Di Spam`, `Nothing`, `Indonesia`, `https://github.com/Xyroinee/Clara-MD`, `Hanya Bot Pasaran Yang Sering Error`]
+    [`${nomorwa}`, `Azka`, `Developer Bot Inc`, `Ponsel`, `Streliz992@gmail.com`, `Washington - USA`, `https://s.id/azkaazhrzlva`, `Just Copy Paster`]
   ], m)
-  await conn.sendFile(m.chat, "https://xyroinee.github.io/audio/audio_ownerku.mp3", 'kuru.mp3', null, m, true, {
-		type: "audioMessage",
-		ptt: true,
-	})
-  } 
-
+}
 handler.help = ['owner', 'creator']
 handler.tags = ['main']
 handler.command = /^(owner|creator)/i
